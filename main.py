fcf7985ac7551595202e18def657f46c96ede6a8
HoiTongW <hoitong1207@gmail.com>
2025-10-23 02:53:07 +0000

    20251023

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from typing import Dict, Any, List
+
 app = FastAPI()
+
+items: List[Dict[str, Any]] = []
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items")
+def read_items():
+    return items
+
+@app.post("/item")
+def create_item(item: Dict[str, Any]):  # 明确指定为字典
+    id = len(items) + 1
+    item["id"] = id
+    items.append(item)
+    return item
